93d2a6bda7911625f08be29c55e4621b33e2939f
Fernanda <fernandagranoziocastro@gmail.com>
2024-03-28 17:53:05 -0300

    Projeto app de Transporte

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  km='';
+  motorista=0;
+  app=0;
+  precofinal=0;
+
   constructor() {}
+
+
+
+  Calcular(){
+    this.precofinal=parseFloat(this.km)*3.5;
+
+    if (this.precofinal>150) {
+      this.motorista=this.precofinal*0.8;
+      this.app=this.precofinal*0.2;
+
+    } else {
+      this.motorista=this.precofinal*0.75;
+      this.app=this.precofinal*0.25;
+
+    }
+  }
 
 }
